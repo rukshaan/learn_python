13fa161dcace15ae322d321e9a2972c3a2a18988
Rukshan <shaanruk0309@gmail.com>
2025-02-18 15:34:29 +0530

    #3_flask templates &html

--- Flask/firstapp/app.py
+++ Flask/firstapp/app.py
@@ -1,14 +1,33 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
-app=Flask(__name__)
+app=Flask(__name__,template_folder='templates')
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    myList=[10,20,30,40,50]
+    return render_template('index.html',myList=myList)
+@app.route('/other')
+def other():
+    address="Mathagal"
+    return render_template('other.html',address=address)
 
-# @app.route('/hello')
-# def hello():
-#     name="Rukshan"
-#     return f"<h1>Hello {name}!!!</h1>"
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    
+    return s[::-1]
+@app.template_filter('repeat')
+def repeat(s,times=3):
+    return s*times
+
+
+
+@app.route('/hello',methods=['GET','POST','PUT'])
+def hello():
+    return "<h1>Hello World!!!</h1>"   
+      
+
+
+
 @app.route("/greet/<name>")
 def greet(name):
     return f"<h1>Hello {name}!!!</h1>"
